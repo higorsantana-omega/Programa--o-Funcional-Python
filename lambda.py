3ad70bffdd35d2cafd5105b6b5c4573504bb08a3
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-16 19:20:13 -0300

    com map

--- lambda.py
+++ lambda.py
@@ -5,3 +5,13 @@
 mult = lambda x: x * 2
 print(mult(10))
 
+# Com map
+compras = (
+    {'Qtd': 2, 'Preço': 1.99},
+    {'Qtd': 5, 'Preço': 3.99}
+)
+
+total = tuple(map(lambda compra: compra['Qtd'] * compra['Preço'], compras))
+
+print('Preço totais', list(total))
+print('Total: ', sum(total))
